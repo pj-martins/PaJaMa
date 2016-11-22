99f10dbf2eb7e625a0b9123f338c203a286a28b0
PJ Martins <pj_martins@hotmail.com>
2016-11-21 17:04:08 -0800

    delete js generated from typescript

--- Recipes/Web/Angular2/src/app/recipes/recipe.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Recipe = (function () {
-    function Recipe() {
-    }
-    return Recipe;
-}());
-exports.Recipe = Recipe;
-//# sourceMappingURL=recipe.js.map
--- Recipes/Web/Angular2/src/app/shared/constants/appsettings.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.AppSettings = {
-    API_ENDPOINT: 'http://localhost:50554/api/'
-};
-//# sourceMappingURL=appsettings.js.map
