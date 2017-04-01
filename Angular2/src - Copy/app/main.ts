85061de8d3fd74aff6fde3a1157f87063790d6ec
PJ Martins <pj_martins@outlook.com>
2017-04-01 07:30:40 -0700

    cleanup

--- Angular2/src - Copy/app/main.ts
+++
@@ -1,7 +0,0 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import { enableProdMode } from '@angular/core';
-
-// enableProdMode();
-
-platformBrowserDynamic().bootstrapModule(AppModule);
--- Angular2/src - Copy/pajama/pipes/to-camel-case.pipe.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'toCamelCase' })
-export class ToCamelCasePipe implements PipeTransform {
-	transform(value: string) {
-		let regex = /([A-Z])/g;
-		return value.replace(regex, " $1");
-	}
-}
